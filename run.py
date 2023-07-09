6a445a5594f5bd5a9cd6c31475e90e69a0bd60c5
Nirmal Subramani <kumar.n.subramani@gmail.com>
2023-07-09 14:51:51 +0000

    Add questions/ options / answers and display question & options

--- run.py
+++ run.py
@@ -5,8 +5,33 @@ player_name = input ("Please enter you name: ")
 playquiz = input (f"{player_name},Do you want to play the quiz?: ")
 if playquiz == "yes":
     print ("Lets play!!!!")
+     
 else:
     quit()
+questions = ("What's national bird of India? " ,
+             "What's the national sport of India?" ,
+             "What's the national Animal of India? "
+            )
+
+options = (("1. Peacock", "2. Pigeon", "3. Swan" ),
+           ("1. Football", "2. Cricket", "3. Hockey"),
+           ("1. Lion", "2. Tiger", "3. elephant"))
+
+answers = ("1", "3", "2")
+
+score = 0
+question_index = 0
+
+ 
+for question in questions: 
+    print(question)
+
+    for option in options[question_index]:
+        print(option)
+    question_index += 1
+ 
+
+
     
 
 
