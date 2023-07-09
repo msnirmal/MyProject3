315987f5160235f0bdfde1c26c277468ed23f6e1
Nirmal Subramani <kumar.n.subramani@gmail.com>
2023-07-09 14:03:21 +0000

    initial commit

--- run.py
+++ run.py
@@ -1,1 +1,19 @@
+print ("Welcome the quiz!!")
+print ("This is a general knowledge quiz")
+
+player_name = input ("Please enter you name: ")
+playquiz = input (f"{player_name},Do you want to play the quiz?: ")
+if playquiz == "yes":
+    print ("Lets play!!!!")
+else:
+    quit()
+    
+
+
+
+
+
+ 
+
+
 
